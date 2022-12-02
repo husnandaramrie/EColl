55f63836ffa55fdea4d4d1b2f6decd7b035bc34d
husnandar amrie <husnandar@outlook.com>
2022-12-02 14:47:46 +0700

    Revisi Close Pin

--- app/Http/Controllers/PinTransaksiController.php
+++ app/Http/Controllers/PinTransaksiController.php
@@ -271,7 +271,7 @@ class PinTransaksiController extends Controller
                                 throw $th;
                             }
                             // @dd($index, $jumdata);
-                            if ($datarow['code'] == 200 && $index >= $jumdata) {
+                            if ($datacbs['code'] == "00" && $index >= $jumdata) {
                                 // return redirect()->route('admin.pintransaksi')->with("success", "Pin Berhasil Di Close");
                                 try {
                                     $bodypin = [
