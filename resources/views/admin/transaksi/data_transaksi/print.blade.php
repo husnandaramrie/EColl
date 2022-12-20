9069e051a76b1ae1835e076c55d9bed3aca70ea7
husnandar amrie <husnandar@outlook.com>
2022-12-20 21:21:07 +0700

    Update Print Transaksi Final

--- resources/views/admin/transaksi/data_transaksi/print.blade.php
+++ resources/views/admin/transaksi/data_transaksi/print.blade.php
@@ -10,26 +10,7 @@
 
 @section('content')
 <div class="row">
-    {{-- @if (Session::has('success'))
-    <div class="col-12">
-        <div class="alert alert-success">{{Session::get('success')}}</div>
-    </div>
-	
-    @endif
-    @if (Session::has('error'))
-    <div class="col-12">
-        <div class="alert alert-danger">{{Session::get('error')}}</div>
-    </div>
-    @endif --}}
-
 	<div class="col-12">
-		{{-- <div class="card"> --}}
-			{{-- <div class="card-header"> --}}
-				{{-- <a href="{{route('admin.data_transaksi.addView')}}" class="btn btn-primary btn-sm">Filter Transaksi</a>
-				<a href="{{route('admin.data_transaksi.addView')}}" class="btn btn-primary btn-sm">Cetak</a> --}}
-			{{-- </div> --}}
-
-			{{-- <div class="card-body table-responsive"> --}}
 				<table id="dataTable0" class="table table-sm table-borderless">
 				<thead>
 				@if($data_user['code'] == '200')
@@ -44,7 +25,7 @@
 						<th colspan="9" class="text-center">{{ $user['clientname'] }}</th>
 					</tr>
 					<tr>
-						<th colspan="9" class="text-center">Petugas {{ $user['name'] }}</th>
+						<th colspan="9" class="text-center text-capitalize">Petugas {{ $user['name'] }}</th>
 					</tr>
 				@endforeach
 				@endif
@@ -63,8 +44,6 @@
 				  <th>Rekening</th>
 				  <th>Nama</th>
 				  <th>Jumlah</th>
-				  {{-- <th>Status</th> --}}
-				  {{-- <th>Aksi</th> --}}
 				</tr>
 				</thead>
 				<tbody>
@@ -81,12 +60,8 @@
 						<td>{{ $data['tipe'] }}</td>
 						<td>{{ $data['refno'] }}</td>
 						<td>{{ $data['norek'] }}</td>
-						<td>{{ $data['name'] }}</td>
+						<td class="text-capitalize">{{ $data['name'] }}</td>
 						<td class="d-flex justify-content-end">{{ number_format($data['amount'],0) }}</td>
-						{{-- <td>{{ $data['status'] }}</td> --}}
-						{{-- <td>
-							<a href="{{route('admin.data_transaksi.destroyDataTrans', $data['refno'])}}" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
-						</td> --}}
 						</tr>
 					@endforeach
 					@endif
@@ -110,51 +85,23 @@
 					@endif
 				</tbody>
 				</table>
-			{{-- </div> --}}
-		{{-- </div> --}}
+				@if ($data_saldo['code']=='200')
+					@foreach ($data_saldo['data'] as $saldo)
+						<table class="table table-borderless table-sm">
+							<thead>
+								<tr>
+									<th>Mengetahui Petugas</th>
+								</tr>
+								<tr>
+									<th></th>
+								</tr>
+								<tr>
+									<th class="text-capitalize">{{ $saldo['username'] }}</th>
+								</tr>
+							</thead>
+						</table>
+					@endforeach
+				@endif
 	</div>
 </div>
-
-{{-- @stop --}}
-{{-- @push('js') --}}
-<!-- DataTables -->
-{{-- <script src="{{ asset('templates/backend/AdminLTE-3.0.1') }}/plugins/datatables/jquery.dataTables.js"></script>
-<script src="{{ asset('templates/backend/AdminLTE-3.0.1') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
-<script src="{{ asset('templates/backend/AdminLTE-3.0.1') }}/plugins/jquery/sum().js"></script>
-<script>
-  $(function () {
-    var table = $('#dataTable1').DataTable({
-		footerCallback: function (row, data, start, end, display) {
-            var api = this.api();
- 
-            // Remove the formatting to get integer data for summation
-            var intVal = function (i) {
-                return typeof i === 'string' ? i.replace(/[\$,]/g, '') * 1 : typeof i === 'number' ? i : 0;
-            };
-
-			
-            // Total over all pages
-            total = api
-                .column(8)
-                .data()
-                .reduce(function (a, b) {
-                    return intVal(a) + intVal(b);
-                }, 0);
- 
-            // Total over this page
-            pageTotal = api
-                .column(8, { page: 'current' })
-                .data()
-                .reduce(function (a, b) {
-                    return intVal(a) + intVal(b);
-                }, 0);
- 
-            // Update footer
-            // $(api.column(8).footer()).html('Rp. ' + new Intl.NumberFormat().format(pageTotal) + ' (Rp. ' + Intl.NumberFormat().format(total) + ' total)');
-			$(api.column(8).footer()).html('Rp. ' + new Intl.NumberFormat().format(pageTotal));
-        },
-	});
-   });
- </script>
-@endpush --}}
 @stop
